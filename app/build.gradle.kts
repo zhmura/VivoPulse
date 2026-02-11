f7475de8c10c2295aa7dd6fff736d5caff5551fc
ext.siarhei.zhmura <ext.siarhei.zhmura@globallogic.com>
2026-02-11 13:13:52 +0100

    release v1.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 29
         targetSdk = 35
         versionCode = 11
-        versionName = "1.0.14"
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
